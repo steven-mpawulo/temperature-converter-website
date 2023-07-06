f665cfb1baf72fecb76dafbe2cf60c44e0546da1
steven-mpawulo <stevenmpawulo@gmail.com>
2023-07-06 15:01:26 +0300

    develops user interface

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header></header>
+      <nav></nav>
+      <main>
+        <div className="container">
+          <div className="first-row">
+          <form action="post">
+          <div className="temperature-number-container">
+          <label htmlFor="temperature-number">Degrees</label><br />
+                <input type="number" id='temperature-number' name='temperature-number' />
+          </div>
+
+          <div className="temperature-type-container">
+          <label htmlFor="temperature-type">Type</label><br />
+                <select name="temperature-type" id="temperature-type">
+                  <option value="Celcius">Celcius</option>
+                  <option value="Fahrenheit">Fahrenheit</option>
+                </select>
+          </div>
+            <div className="convert-button-container">
+            <button type='submit' id='convert' className='convert'>Convert</button>
+            </div>
+            </form>
+          </div>
+
+          <div className="second-row">
+          <div className="results-container">
+              <p>Results</p>
+              <p><strong>Answer</strong></p>
+            </div>
+          </div>
+      
+        </div>
+            
+      </main>
+      <footer></footer>
     </div>
   );
 }
