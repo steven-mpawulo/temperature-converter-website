af924f7c193c58a9743c39b432dc11aa6234d8c5
steven-mpawulo <stevenmpawulo@gmail.com>
2023-07-10 15:03:15 +0300

    works on conversion method

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [degree, setDegree] = useState(0);
+  const [temperatureType, setTemperatureType] = useState("Celcius");
+  const [convertedTemperature, setConvertedTemperature] = useState('');
+
+  const handleInputChange = (event) => {
+    setDegree(event.target.value);
+    console.log(event.target.value);
+  }
+
+  const handleSelectChange = (event) => {
+    setTemperatureType(event.target.value);
+    console.log(event.target.value);
+  }
+
+  const handleButtonClick = (degree, temperatureType) => {
+    if (temperatureType === "Celcius"){
+      let result = (degree * 9/5) + 32;
+      setConvertedTemperature(`${result} \u00B0 F`);
+    } else if(temperatureType === "Fahrenheit") {
+      let result = (degree - 32) * (5 / 9);
+      console.log(`${degree} to be converted to celcius`);
+      setConvertedTemperature(`${result} \u00B0 C`);
+    }
+    else {
+    }
+  }
   return (
     <div>
       <header></header>
@@ -9,34 +36,39 @@ function App() {
       <main>
         <div className="container">
           <div className="first-row">
-          <form action="post">
-          <div className="temperature-number-container">
-          <label htmlFor="temperature-number">Degrees</label><br />
-                <input type="number" id='temperature-number' name='temperature-number' />
-          </div>
+            <form action='post' onSubmit={(event) => {
+              event.preventDefault();
+              handleButtonClick(degree, temperatureType);
+            }}>
+              <div className="temperature-number-container">
+                <label htmlFor="temperature-number">Degrees</label><br />
+                <input type="number" id='temperature-number' name='temperature-number' value={degree} onChange={handleInputChange} />
+              </div>
 
-          <div className="temperature-type-container">
-          <label htmlFor="temperature-type">Type</label><br />
-                <select name="temperature-type" id="temperature-type">
+              <div className="temperature-type-container">
+                <label htmlFor="temperature-type">Type</label><br />
+                <select name="temperature-type" id="temperature-type" onChange={handleSelectChange} value={temperatureType}>
                   <option value="Celcius">Celcius</option>
                   <option value="Fahrenheit">Fahrenheit</option>
                 </select>
-          </div>
-            <div className="convert-button-container">
-            <button type='submit' id='convert' className='convert'>Convert</button>
-            </div>
+              </div>
+              <div className="convert-button-container">
+                <button type='submit' id='convert' className='convert' onClick={() => {
+                  handleButtonClick(degree, temperatureType);
+                }}>Convert</button>
+              </div>
             </form>
           </div>
 
           <div className="second-row">
-          <div className="results-container">
+            <div className="results-container">
               <p>Results</p>
-              <p><strong>Answer</strong></p>
+              <p><strong>{convertedTemperature}</strong></p>
             </div>
           </div>
-      
+
         </div>
-            
+
       </main>
       <footer></footer>
     </div>
